cb3ea96cf8f0de2def929680de5d2a79a0340b42
vlad-fvv <vladfomin2008@gmail.com>
2022-03-24 23:05:17 +0300

    Блокировка кнопки Play

--- game_functions.py
+++ game_functions.py
@@ -38,7 +38,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
                       bullets,
                       mouse_x, mouse_y):
     """Запускает новую игру при нажатии кнопки Play"""
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
         # Сброс игровой статистики
         stats.reset_stats()
         stats.game_active = True
