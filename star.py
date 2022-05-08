c5c03bd334f6b369caad9fafff430c4993f33a73
vlad-fvv <vladfomin2008@gmail.com>
2022-05-08 13:07:14 +0300

    скорость звезда зависит от величины: большие движутся быстрее. прозрачность звезд зависит от величины: чем дальше звезда, тем она прозрачнее

--- star.py
+++ star.py
@@ -11,8 +11,6 @@ class Star(Sprite):
         """создает звезду со случаным изображением из списка"""
         super(Star, self).__init__()
         self.ai_settings = ai_settings
-        # скорость звезд
-        self.star_speed = int(self.ai_settings.star_speed * randint(0, 3))
 
         file_name = 'stars'
 
@@ -25,18 +23,27 @@ class Star(Sprite):
         ]
 
         # одна из пяти картинок
-        sf = images[randint(0, 4)]
+        self.sf = images[randint(0, 4)]
 
         # уменьшение картики нв случайную величину от 1 до 5
-        zoom = randint(4, 6)
+        self.zoom = randint(4, 6)
 
         # готовая картинка
-        self.image = pygame.transform.scale(sf,
-                                            (sf.get_width() // zoom,
-                                             sf.get_height() // zoom)
+        self.image = pygame.transform.scale(self.sf,
+                                            (self.sf.get_width() // self.zoom,
+                                             self.sf.get_height() // self.zoom)
                                             )
-        self.rect = self.image.get_rect()
+        # скорость и затемнение звезд
+        if self.zoom == 6:
+            self.star_speed = 1
+            self.image.set_alpha(100)
+        elif self.zoom == 5:
+            self.star_speed = 2
+            self.image.set_alpha(200)
+        else:
+            self.star_speed = 3
 
+        self.rect = self.image.get_rect()
         # Каждая новая звезда появляется в левом верхнем углу экрана.
         self.rect.x = 0
         self.rect.y = 0
